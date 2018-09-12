c486fbfc737558539af8b2cc0a18d3db20923450
AGCH <anastasiyache@gmail.com>
2018-09-12 17:23:29 +0300

    with state and props

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import Letters from './components/Letters.js';
 import Score from './components/Score.js';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      letterStatus: this.generateLetterStatuses()
+    }
+  }
+  generateLetterStatuses(){
+    let letterStatus = {};
+    for(let num = 65; num < 90; num++) {
+        letterStatus[String.fromCharCode(num)] =false;
+    };
+    return letterStatus 
+    }
   render() {
   return (
     <div>
   <div><Score /></div>
-  <div><Letters /></div>
+  <div><Letters letterStatus={this.state.letterStatus} /></div>
   </div>
   );
   }
--- src/components/Letters.js
+++ src/components/Letters.js
@@ -2,28 +2,20 @@ import React, { Component } from 'react';
 import Letter from './Letter.js';
 class Letters extends Component {
 
-    generateLetterStatuses(){
-    let letterStatus = {};
-    for(let num = 65; num < 90; num++) {
-        letterStatus[String.fromCharCode(num)] =false;
-    };
-    return letterStatus 
-    }
-
-    generateLetterTags(ls)
-    {
-    return Object.keys(ls) ;
-    }
     
+    generateLetterTags(letterStatus){
+        // let letterTags = [], num=0;
+        return Object.keys(letterStatus).map((l, i) => {
+            return (<Letter letter={l} key={l}/>)   //the power of JSX!
+        })
+    }
     render() {
-    let letterStatuses = this.generateLetterStatuses();
-    const letters = this.generateLetterTags(letterStatuses);
+    // let letterStatuses = this.generateLetterStatuses();
     return (
         <div>
-            <div>Letters will be here</div>
+            <div>Available letters</div>
             {/* <div><Letter letter={letter} /></div> */}
-            <div>{letters}</div>
-            <div>{console.log(this.generateLetterStatuses())}</div>
+            <div>{this.generateLetterTags(this.props.letterStatus)}</div>
         </div>
     );
 }

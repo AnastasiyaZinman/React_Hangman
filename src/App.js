feb4c664503c29ebf9fb9588a863c8e6c0d83ffa
AGCH <anastasiyache@gmail.com>
2018-09-14 23:57:04 +0300

    Events ex1, win or game-over

--- src/App.js
+++ src/App.js
@@ -11,14 +11,17 @@ class App extends Component {
       letterStatus: this.generateLetterStatuses(),
       score: 100,
       word: 'CALM',
+      guessedLetters:0,
       hint: "Your ideal mood when coding."
     }
   }
-  
+
   reduceScore = (l) => {
     let newScore;
     if (this.state.word.indexOf(l)!==-1)
-    { newScore = this.state.score + 5}
+    { newScore = this.state.score + 5;
+      this.state.guessedLetters++
+    }
     else {
     newScore = this.state.score - 20}
     this.setState({ score: newScore })
@@ -33,6 +36,7 @@ class App extends Component {
     
     selectLetter = (l) => {
       this.reduceScore(l);
+      console.log(l);
       // let newletterStatus = this.state.letterStatus;
       let newletterStatus = {...this.state.letterStatus};
       console.log("newletterStatus",newletterStatus);
@@ -40,8 +44,12 @@ class App extends Component {
       this.setState({ letterStatus: newletterStatus })
     }
   render() {
-  return (
+    if (this.state.score<=0) return <div className="game-over">GAME OVER</div>
+    else if (this.state.score>0 && this.state.guessedLetters===this.state.word.length)
+    return <div className="game-win">WIN!</div>
+    return (
     <div>
+      <span>{this.state.notGuessedLetters}</span>
   <div className="score"><Score score={this.state.score}/></div>
   <div><Letters letterStatus={this.state.letterStatus} selectLetter={this.selectLetter} /></div>
   <div><Solution word={this.state.word} hint={this.state.hint}  letterStatus={this.state.letterStatus}/></div>
